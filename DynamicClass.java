676eabb9dddae32060c8a842daaf5e64e012ae42
Girish Pandit <pandit.girish@gmail.com>
2025-08-18 17:30:39 -0400

    Update DynamicClass.java

--- DynamicClass.java
+++ DynamicClass.java
@@ -36,3 +36,23 @@ public class DynamicServletRegistrar {
         };
     }
 }
+
+
+
+
+
+
+
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TomcatCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addConnectorCustomizers(connector -> connector.setEnableLookups(true));
+    }
+}
+
